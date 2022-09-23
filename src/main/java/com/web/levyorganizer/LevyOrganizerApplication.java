08748e52186ac415be17d0a3610b71f797eb224b
Saransh Tyagi <tyagisaransh90@gmail.com>
2022-09-23 22:26:41 +0530

    Project Structure

---
+++ src/main/java/com/web/levyorganizer/LevyOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.web.levyorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevyOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevyOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/levyorganizer/LevyOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.levyorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevyOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
